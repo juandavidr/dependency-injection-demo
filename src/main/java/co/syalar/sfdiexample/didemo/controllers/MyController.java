b3e2ecfa5d6d84f2ee1745c46bb3d9debd164fd1
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2020-04-27 21:08:12 -0500

    Added modifications

--- src/main/java/co/syalar/sfdiexample/didemo/controllers/MyController.java
+++ src/main/java/co/syalar/sfdiexample/didemo/controllers/MyController.java
@@ -1,5 +1,6 @@
 package co.syalar.sfdiexample.didemo.controllers;
 
+import co.syalar.sfdiexample.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -8,8 +9,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-    public String hello(){
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String hello() {
         System.out.println("Hello");
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
